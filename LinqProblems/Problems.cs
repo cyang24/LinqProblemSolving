6d4358eff251f7d2e8316a8f2593919bb736df72
cyang24 <yg00@yahoo.com>
2018-07-22 19:32:18 -0500

    problem 1 finished

---
+++ LinqProblems/Problems.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProblems
+{
+    class Problems
+    {
+        
+        public void FindPhraseInString(List<string> Words, string phrase)
+        {
+            var resultList = Words.Where(w => w.Contains(phrase));
+            foreach (string result in resultList)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+    }
+}
---
+++ LinqProblems/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problems problems = new Problems();
+
+
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            problems.FindPhraseInString(words, "th");
+
+
+
+
+
+
+            Console.ReadLine();
+          
+        }
+    }
+}
